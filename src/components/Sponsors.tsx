3d1d6619bc0b980e800f0d7e1042e27b96b42bee
darrenjng <darrendotng@gmail.com>
2025-06-03 10:25:44 -0700

    added padsys sponsor

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,6 +1,7 @@
 // pages/sponsors.tsx
 import Image from 'next/image';
 import nsf from '@/images/nsf_no_text.jpeg'
+import padsys from '@/images/padsys_logo.png'
 import ucm from '@/images/ucm_logo.png'
 import { Button } from '@/components/Button'
 
@@ -9,22 +10,30 @@ export function Sponsors() {
     <div className="flex flex-col items-center justify-center bg-white px-5 text-center">
       <h1 className="text-4xl font-bold text-gray-800 mb-6">Our Sponsors</h1>
 
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-3 gap-10">
         {/* Sponsor 1 */}
         <div className="flex justify-center">
           <Image
             src={nsf}
-            alt="Sponsor 1"
-            width={220}
+            alt="NSF"
+            width={205}
             className="object-contain"
           />
         </div>
 
-        {/* Sponsor 2 */}
+        <div className="relative h-50 w-50 overflow-hidden rounded-full">
+            <Image
+              className="flex justify-center"
+              src={padsys}
+              alt="PADSYS"
+            />
+        </div>
+
+        {/* Sponsor 3 */}
         <div className="flex justify-center">
           <Image
             src={ucm}
-            alt="Sponsor 2"
+            alt="UCM"
             width={200}
             className="object-contain"
           />
